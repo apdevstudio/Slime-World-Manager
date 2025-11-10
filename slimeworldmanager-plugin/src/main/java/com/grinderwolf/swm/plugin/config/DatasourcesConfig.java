8f192a04ea2e7e25e2a67ad87e4cd698ef942bb1
AP Network <asspixelnetwork@gmail.com>
2025-11-10 02:45:00 -0500

    Revert "Add support for custom MongoDB connection URI (#129)"
    This reverts commit 59443d6621c86b2bb245dd70078767dfd173dfdb.

--- slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/config/DatasourcesConfig.java
+++ slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/config/DatasourcesConfig.java
@@ -42,8 +42,6 @@ public class DatasourcesConfig {
 
         @Setting("database") private String database = "slimeworldmanager";
         @Setting("collection") private String collection = "worlds";
-
-        @Setting("uri") private String uri = "";
     }
 
     @Getter
